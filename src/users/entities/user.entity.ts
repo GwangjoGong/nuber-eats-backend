fe0fb9e5b8cd50dc83e2a46efd28dcc5a8254b2a
Gwangjo Gong <dwc05101@kaist.ac.kr>
2020-12-17 13:58:51 +0900

    Added missing validators

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -8,7 +8,7 @@ import { CoreEntity } from 'src/common/entities/core.entity';
 import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { InternalServerErrorException } from '@nestjs/common';
-import { IsEmail, IsEnum, IsString } from 'class-validator';
+import { IsBoolean, IsEmail, IsEnum, IsString } from 'class-validator';
 
 enum UserRole {
   Client,
@@ -34,6 +34,7 @@ export class User extends CoreEntity {
 
   @Column({ default: false })
   @Field(is => Boolean, { defaultValue: false })
+  @IsBoolean()
   verified: boolean;
 
   @Column({ type: 'enum', enum: UserRole })
